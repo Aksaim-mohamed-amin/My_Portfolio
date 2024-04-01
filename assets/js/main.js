a8543aa3adda2ab00df80399f97317e5f9f1d149
aksaim mohamed amin <aksaimmohamedamin@gmail.com>
2024-04-01 16:32:54 +0000

    header

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.documentElement.style.setProperty('--bg-color', 'white');
+    document.documentElement.style.setProperty('--main-textColor', 'black');
+    document.documentElement.style.setProperty('--secendry-textColor', '#2a2b2c');
+
+    // --custom-pink: #E06B74;
+    // --custom-purpel: #C778DD;
+    // --custom-green: #98C379;
+    // --costum-yellow: #E5C07A;
+    // --custom-blue: #62AEEF;
+    // --custom-turquoise: #55B6C2;
+}
